9746c77f7d66b2f7fdd05c23382d110a877160ec
Edy Kantor <edycoleee@gmail.com>
2021-04-16 07:46:40 +0700

    satu

---
+++ admin-crud/src/App.js
@@ -0,0 +1,11 @@
+//App.js
+
+function App() {
+  return (
+    <div>
+      <h1>ADMIN SITE</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ coba-crud/src/App.js
@@ -0,0 +1,11 @@
+//App.js 
+
+function App() {
+  return (
+    <div>
+      <h1>PUBLIC SITE</h1>
+    </div>
+  );
+}
+
+export default App;
