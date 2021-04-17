0857120e9164b48990f8ea56edd8e12af51318ed
Edy Cole <edycoleee@gmail.com>
2021-04-17 07:16:09 +0700

    latihan6

--- admin-crud/src/App.js
+++ admin-crud/src/App.js
@@ -4,7 +4,8 @@
 //import Latihan2 from "./Latihan2";
 //import Latihan3 from "./Latihan3";
 //import Latihan4 from "./Latihan4";
-import Latihan5 from "./Latihan5";
+//import Latihan5 from "./Latihan5";
+import Latihan6 from "./Latihan6";
 
 function App() {
   return (
@@ -15,7 +16,8 @@ function App() {
       {/* <Latihan2 /> */}
       {/* <Latihan3 /> */}
       {/* <Latihan4 /> */}
-      <Latihan5 />
+      {/* <Latihan5 /> */}
+      <Latihan6 />
     </div>
   );
 }
---
+++ admin-crud/src/Latihan6.js
@@ -0,0 +1,211 @@
+//src/Latihan6.js
+import React, { useState } from "react";
+//import firebase
+import { db, Firebase } from "./firebase";
+
+//setting ini jika menggunakan emulator firestore
+//comment jika mau langsung koneksi ke cloud firebase
+//settings({ host: "localhost:8080", ssl: false });
+
+function Latihan6() {
+  //state untuk add data
+  const [nmCollection, setNmCollection] = useState("");
+  const [kolom1, setKolom1] = useState("");
+  const [kolom2, setKolom2] = useState("");
+  const [kolom3, setKolom3] = useState("");
+
+  //state untuk data dari hasil pembacaan
+  const [dataCollection, setDataCollection] = useState([]);
+
+  //state untuk edit data
+  const [edtnmDokumen, setEdtNmDokumen] = useState("");
+
+  //button simpan fuction simpan dokumen auto id (ADD)
+  function onSimpanDok() {
+    if (nmCollection === "") return console.log("Nama Koleksi Kosong");
+    if (kolom1 === "") return console.log("Kolom1 Kosong");
+    if (kolom2 === "") return console.log("Kolom2 Kosong");
+    console.log(nmCollection, kolom1, kolom2);
+    db.collection(nmCollection)
+      .add({
+        kolom1: kolom1,
+        kolom2: kolom2,
+      })
+      .then((doc) => {
+        getAllCol();
+        setKolom1("");
+        setKolom2("");
+        console.log("Added doc with ID : ", doc.id);
+      })
+      .catch((error) => {
+        console.error("Error writing document: ", error);
+      });
+  }
+
+  function onSimpanDokDgIdMerge() {
+    if (nmCollection === "") return console.log("Nama Koleksi Kosong");
+    if (edtnmDokumen === "") return console.log("Nama Dokumen Kosong");
+    if (kolom3 === "") return console.log("Kolom3 Kosong");
+    console.log(nmCollection, edtnmDokumen, kolom1, kolom2);
+    db.collection(nmCollection)
+      .doc(edtnmDokumen)
+      .set(
+        {
+          kolom3: kolom3,
+        },
+        { merge: true }
+      )
+      .then(() => {
+        getAllCol();
+        console.log("Document successfully written!");
+      })
+      .catch((error) => {
+        console.error("Error writing document: ", error);
+      });
+  }
+
+  //button membaca semua data dari collection
+  const getAllCol = () => {
+    if (nmCollection === "") return console.log("Nama Koleksi Kosong");
+    console.log("Get Once All Data Collection", nmCollection);
+    db.collection(nmCollection)
+      .get()
+      .then((firecol) => {
+        const data = firecol.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        console.log("Get All Data Collection :", data);
+        setDataCollection(data);
+      })
+      .catch((error) => console.error("Error Get Data :", error));
+  };
+
+  //button mengambil data yang akan di edit
+  function onEditDok(row) {
+    console.log("Edit Dok :", row);
+    //memasukkan data kedalam state edit
+    setEdtNmDokumen(row.id);
+    setKolom3(row.kolom3)
+  }
+
+  //button utk delete data
+  function onDeleteDok(id_del) {
+    console.log("Delete id dok :", id_del);
+  }
+
+  //button utk delete field
+  function onDeleteField() {
+    console.log("Delete kolom3 dalam dok :", edtnmDokumen);
+    if (nmCollection === "") return console.log("Nama Koleksi Kosong");
+    if (!edtnmDokumen) return console.log("Nama Dokumen Kosong");
+    console.log(nmCollection, edtnmDokumen);
+    db.collection(nmCollection)
+      .doc(edtnmDokumen)
+      .update({
+        kolom3: Firebase.firestore.FieldValue.delete(),
+      })
+      .then(() => {
+        getAllCol();
+        setKolom3("");
+        console.log("Deleted doc ");
+      })
+      .catch((error) => {
+        console.error("Error update document: ", error);
+      });
+  }
+
+  return (
+    <div>
+      <h3>Latihan 6 Latihan Add Delete KOlom dalam Dok</h3>
+      {/* -------------------add data------------------------- */}
+      <fieldset>
+        <legend>Add Data :</legend>
+        <label htmlFor="nmCollection">Nama Collection :</label>
+        <input
+          style={{ marginLeft: "1em" }}
+          type="text"
+          name="nmCollection"
+          placeholder="Isi Nama Collection"
+          onChange={(e) => setNmCollection(e.target.value)}
+        />{" "}
+        <button onClick={getAllCol}>LIHAT COLL</button>
+        <br></br>
+        <br></br>
+        <label htmlFor="kolom1">Kolom1 :</label>
+        <input
+          style={{ marginLeft: "1em" }}
+          type="text"
+          name="kolom1"
+          placeholder="Isi Kolom1"
+          onChange={(e) => setKolom1(e.target.value)}
+        />
+        <br></br>
+        <br></br>
+        <label htmlFor="kolom2">Kolom2 :</label>
+        <input
+          style={{ marginLeft: "1em" }}
+          type="text"
+          name="kolom2"
+          placeholder="Isi Kolom2"
+          onChange={(e) => setKolom2(e.target.value)}
+        />
+        <br></br>
+        <br></br>
+        <button onClick={onSimpanDok}>SIMPAN DOK</button>
+      </fieldset>
+      {/* -------------------table data------------------------- */}
+      <fieldset>
+        <legend>Table Firestore </legend>
+        <table>
+          <thead>
+            <tr>
+              <th>kolom 1</th>
+              <th>kolom 2</th>
+              <th>kolom 3</th>
+              <th>EDIT</th>
+              <th>DELETE</th>
+            </tr>
+          </thead>
+          <tbody>
+            {dataCollection?.map((row) => (
+              <tr key={row.id} style={{ height: "2em" }}>
+                <td>{row.kolom1}</td>
+                <td>{row.kolom2}</td>
+                <td>{row.kolom3}</td>
+                <td>
+                  <button onClick={() => onEditDok(row)}>EDIT</button>
+                </td>
+                <td>
+                  <button onClick={() => onDeleteDok(row.id)}>DELETE</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </fieldset>
+      {/* -------------------Kolom data------------------------- */}
+      <br></br>
+      <fieldset>
+        <legend>
+          Add / Delete Field Data inside dokumen {edtnmDokumen || "Kosong"}{" "}
+        </legend>
+        <label htmlFor="kolom3">Kolom3 :</label>
+        <input
+          style={{ marginLeft: "1em" }}
+          type="text"
+          name="kolom3"
+          placeholder="Isi Kolom3"
+          value={kolom3 || ""}
+          onChange={(e) => setKolom3(e.target.value)}
+        />
+        <br></br>
+        <br></br>
+        <button onClick={onSimpanDokDgIdMerge}>ADD FIELD Kolom3</button>{" "}
+        <button onClick={onDeleteField}>DELETE FIELD Kolom3</button>
+      </fieldset>
+    </div>
+  );
+}
+
+export default Latihan6;
